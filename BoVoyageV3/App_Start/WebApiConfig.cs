ca91cfcd886dcae515434ccaa5d4b84ab9e2872c
YoosZ <so_yoos@msn.com>
2018-09-20 12:15:12 +0200

    Ajout de ReferenceLoopHandling.Ignore

--- BoVoyageV3/App_Start/WebApiConfig.cs
+++ BoVoyageV3/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -9,10 +10,12 @@ namespace BoVoyageV3
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
+			// Web API configuration and services
 
-            // Web API routes
-            config.MapHttpAttributeRoutes();
+			GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+
+			// Web API routes
+			config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
