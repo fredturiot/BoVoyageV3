0a9a16c195eff79bd6349f46d26d3e0ffdf67820
YoosZ <so_yoos@msn.com>
2018-09-20 10:24:10 +0200

    Création des Models : AgenceVoyage, Destination et Voyage. Création dans le Context des DBSet associés.

--- BoVoyageV3/Data/BoVoyageContext.cs
+++ BoVoyageV3/Data/BoVoyageContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BoVoyageV3.Models;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -9,5 +10,9 @@ namespace BoVoyageV3.Data
 	public class BoVoyageContext : DbContext
 	{
 		public BoVoyageContext() : base("BoVoyageV3ConnectionString") {}
+
+		public DbSet<AgenceVoyage> AgencesVoyages { get; set; }
+		public DbSet<Destination> Destinations { get; set; }
+		public DbSet<Voyage> Voyages { get; set; }
 	}
 }
