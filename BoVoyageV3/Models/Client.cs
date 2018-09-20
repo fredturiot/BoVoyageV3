23276cf5394f5c8b120c5d2c275a91bfbb84a8f7
YoosZ <so_yoos@msn.com>
2018-09-20 17:04:58 +0200

    Ajout des Models Personne et Client

---
+++ BoVoyageV3/Models/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BoVoyageV3.Models
+{
+	public class Client : Personne
+	{
+		[Required]
+		[Index(IsUnique = true)]
+		[StringLength(60, MinimumLength = 5, ErrorMessage = "L'adresse mail doit avoir de 5 a 60 caracteres")]
+		public string Email { get; set; }
+	}
+}
