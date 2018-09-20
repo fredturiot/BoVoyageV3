c8f417a993098c2f8575f5a2cec0360cb5d7ed18
YoosZ <so_yoos@msn.com>
2018-09-20 10:03:57 +0200

    Ajout de EF6, création de la BD qui doit avoir comme nom "BoVoyageBDV3", création du Context et fichier de configuration des migrations

---
+++ BoVoyageV3/Data/BoVoyageContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BoVoyageV3.Data
+{
+	public class BoVoyageContext : DbContext
+	{
+		public BoVoyageContext() : base("BoVoyageV3ConnectionString") {}
+	}
+}
---
+++ BoVoyageV3/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace BoVoyageV3.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BoVoyageV3.Data.BoVoyageContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(BoVoyageV3.Data.BoVoyageContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
