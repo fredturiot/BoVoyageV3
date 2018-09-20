47af8b370767436f161e135318f88b9f37ce71c7
YoosZ <so_yoos@msn.com>
2018-09-20 10:35:09 +0200

    Ajout des controleurs AgencesVoyages, Destinations et Voyages

---
+++ BoVoyageV3/Controllers/AgencesVoyagesController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BoVoyageV3.Data;
+using BoVoyageV3.Models;
+
+namespace BoVoyageV3.Controllers
+{
+    public class AgencesVoyagesController : ApiController
+    {
+        private BoVoyageContext db = new BoVoyageContext();
+
+        // GET: api/AgencesVoyages
+        public IQueryable<AgenceVoyage> GetAgencesVoyages()
+        {
+            return db.AgencesVoyages;
+        }
+
+        // GET: api/AgencesVoyages/5
+        [ResponseType(typeof(AgenceVoyage))]
+        public IHttpActionResult GetAgenceVoyage(int id)
+        {
+            AgenceVoyage agenceVoyage = db.AgencesVoyages.Find(id);
+            if (agenceVoyage == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(agenceVoyage);
+        }
+
+        // PUT: api/AgencesVoyages/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutAgenceVoyage(int id, AgenceVoyage agenceVoyage)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != agenceVoyage.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(agenceVoyage).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AgenceVoyageExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/AgencesVoyages
+        [ResponseType(typeof(AgenceVoyage))]
+        public IHttpActionResult PostAgenceVoyage(AgenceVoyage agenceVoyage)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.AgencesVoyages.Add(agenceVoyage);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = agenceVoyage.ID }, agenceVoyage);
+        }
+
+        // DELETE: api/AgencesVoyages/5
+        [ResponseType(typeof(AgenceVoyage))]
+        public IHttpActionResult DeleteAgenceVoyage(int id)
+        {
+            AgenceVoyage agenceVoyage = db.AgencesVoyages.Find(id);
+            if (agenceVoyage == null)
+            {
+                return NotFound();
+            }
+
+            db.AgencesVoyages.Remove(agenceVoyage);
+            db.SaveChanges();
+
+            return Ok(agenceVoyage);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AgenceVoyageExists(int id)
+        {
+            return db.AgencesVoyages.Count(e => e.ID == id) > 0;
+        }
+    }
+}
