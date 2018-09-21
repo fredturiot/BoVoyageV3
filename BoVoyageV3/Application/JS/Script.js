6d97b034ea9f3e87124eae4787ee318015503055
YoosZ <so_yoos@msn.com>
2018-09-21 16:52:42 +0200

    Modification de la fonction Load

--- BoVoyageV3/Application/JS/Script.js
+++ BoVoyageV3/Application/JS/Script.js
@@ -2,48 +2,31 @@ function setLoader(element) {
     element.append($('<div class="loader"></div>'));
 }
 
-function GetURLParameter(sParam)
-{
-     var sPageURL = window.location.search.substring(1);
-	var sURLVariables = sPageURL.split('&');
-	for (var i = 0; i < sURLVariables.length; i++)
-	{
-	    var sParameterName = sURLVariables[i].split('=');
-	    if (sParameterName[0] == sParam)
-	    {
-	        return sParameterName[1];
-	    }
-	 }
-}
-
-function Load(controller) {
-        return Promise.resolve($.ajax({
-            url: '/api/'+ controller,           
-            /* success: function (items) {
-                return items.SignedIn;              
-            } */
-        }));
-}
-/* function LoadAjaxData() {
-    var searchString = $('#searchString').val();
-    $.ajax(
-        {
-            method: "GET",
-            url: "" + searchString + 
-        })
-        .done(function (data) {
-            var ul = $('<ul>');
-            data[''].forEach(function (item) {
-                ul.append($('<li>' + item.name + '</li>'));
-            });
-            $('#result').append(ul);
+function GetURLParameter(sParam) {
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) {
+            return sParameterName[1];
         }
-        )
+    }
 }
- */
 
+function Load(controller, id = 0) {
+    if (id === 0) {
+        return Promise.resolve($.ajax({
+            url: '/api/' + controller,
+        }));
+    } else {
+        return Promise.resolve($.ajax({
+            url: '/api/' + controller + '/' + id.toString(),
+        }));
+    }
+}
 
-
-
-
-
+function Erreur(data) {
+    var modelState = data.responseJSON.ModelState;
+    var message = modelState[Object.keys(modelState)[0]][0];
+    alert(message);
+}
